99776a46695824104716ccfa954072c6cdb86cac
aliciacanales <canalesalicia06@gmail.com>
2024-04-17 16:30:43 -0700

    changed the values for cost and labor

--- functions/compute_profit.R
+++ functions/compute_profit.R
@@ -1,8 +1,9 @@
 #..............Profit model..........................
 
-compute_profit <- function(climate_data, labor = 15, cost = 5) {
+compute_profit <- function(climate_data, labor = 50, cost = 15) {
   yield = compute_crop_yield(climate_data, Tcoeff1 = -0.015, Tcoeff2 = -0.0046, Pcoeff1 = -0.07, Pcoeff2 = 0.0043, intercept = 0.28)
 
+  set.seed(123)  
   price = runif(nrow(yield), 1, 10) ## price we get for almond produced 
   
   income = price * yield$yield_anomaly
