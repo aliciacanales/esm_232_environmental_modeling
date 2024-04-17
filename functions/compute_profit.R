7ae89e15e3c88f1dc5cd4b28230ec7ee2259335e
aliciacanales <canalesalicia06@gmail.com>
2024-04-17 15:46:17 -0700

    .

--- functions/compute_profit.R
+++ functions/compute_profit.R
@@ -1,12 +1,12 @@
 #..............Profit model..........................
 
-compute_profit <- function(climate_data, irr = 15, cost = 5) {
+compute_profit <- function(climate_data, labor = 15, cost = 5) {
   yield = compute_crop_yield(climate_data, Tcoeff1 = -0.015, Tcoeff2 = -0.0046, Pcoeff1 = -0.07, Pcoeff2 = 0.0043, intercept = 0.28)
 
   price = runif(nrow(yield), 1, 10) ## price we get for almond produced 
   
   income = price * yield$yield_anomaly
-  total_cost = irr * cost
+  total_cost = labor * cost
   net_yield = round(income - total_cost)
   
   profit = data.frame(year = yield$year, profit = net_yield)
