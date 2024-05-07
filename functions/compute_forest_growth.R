688f709d3acf0db12a13ba8a2546360689bef7ca
aliciacanales <canalesalicia06@gmail.com>
2024-05-06 21:42:53 -0700

    updated function and rmd file for assignment 6

--- functions/compute_forest_growth.R
+++ functions/compute_forest_growth.R
@@ -1,14 +1,10 @@
-compute_forest_growth <- function(r, c, g, K, canopy = 50){
+compute_forest_growth <- function(time, r, c, g, K, threshold = 50){
   
- # inputting if then statement to determine which carbon model to use
-   if (c > canopy){
-     
-     # if carbon is above canopy threshold
-    forest_growth = r * c
-  } else {
-    
-    # if carbon is below canopy threshold
-    forest_growth = g * (1 - c/K)
+  forest_growth <- r * c
+  
+  if (c < threshold){
+    forest_growth <- g * (1 - c/K)
   }
-  return(forest_growth)
+  
+   return(list(forest_growth))
 }
