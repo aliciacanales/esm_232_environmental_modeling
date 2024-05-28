9bc31f2efccef256b7d8f7b233770805b18541ef
aliciacanales <canalesalicia06@gmail.com>
2024-05-28 14:38:20 -0700

    commit recent changes

--- functions/compute_forest_growth.R
+++ functions/compute_forest_growth.R
@@ -1,3 +1,11 @@
+#' Compute Forest Growth
+#'
+#' @param time The time sequence the function will run over
+#' @param c The size of the forest in units of carbon
+#' @param params List of parameters that will calculate forest growth. Should include k, r, g, and threshold
+#'
+#' @return The forest growth using either exponential rate of change or linear rate of change 
+#' 
 compute_forest_growth <- function(time, c, params){
     
   forest_growth <- params$g * (1 - c/params$k)
